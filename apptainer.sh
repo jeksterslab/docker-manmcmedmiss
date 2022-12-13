976b14fd8958d5137c48db41f0fc4104760bce14
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2022-12-13 19:36:29 +0800

    Update apptainer.sh

--- apptainer.sh
+++ apptainer.sh
@@ -2,7 +2,7 @@
 
 # pre TMP ----------------------------------------------------------------------
 TODAY=$(date +"%Y-%m-%d-%H%M")
-TMP_FOLDER=$(mktemp -d -q "$HOME/$TODAY-XXXXXXXX" || exit 1)
+TMP_FOLDER=$(mktemp -d -q "$PWD/$TODAY-XXXXXXXX" || exit 1)
 # ------------------------------------------------------------------------------
 
 APPTAINER_TMPDIR="$TMP_FOLDER" \
